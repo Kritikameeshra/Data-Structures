8fc7afeb5cf25fe207b34663fabada75a73d2d14
Kritika Kumari Mishra <144877304+Kritikameeshra@users.noreply.github.com>
2024-05-07 01:16:38 +0530

    Updated Solution - GfG to GitHub

--- Easy/Print all nodes that don't have sibling/print-all-nodes-that-dont-have-sibling.java
+++ Easy/Print all nodes that don't have sibling/print-all-nodes-that-dont-have-sibling.java
@@ -105,21 +105,33 @@ class GfG {
 
 class Tree
 {
-    ArrayList<Integer> noSibling(Node node)    {
-         // code here
-        ArrayList<Integer> ans = new ArrayList<>();
-        noSibling(node, ans);
-        Collections.sort(ans);
-        if(ans.size() == 0) ans.add(-1);
-        return ans;
+    ArrayList<Integer> noSibling(Node node)
+    {
+        // code here
+        ArrayList<Integer> list = new ArrayList<>();
+        sol(node,list);
+        if(list.size() == 0)
+        {
+            list.add(-1);
+        }
+        Collections.sort(list);
+        return list;
     }
-    
-    void noSibling(Node node, ArrayList<Integer> ans){
-        if(node == null || (node.left == null && node.right == null)) return;
-     
-        if(node.left == null) ans.add(node.right.data);
-        if(node.right == null) ans.add(node.left.data);
-        noSibling(node.left, ans);
-        noSibling(node.right, ans);
+    void sol(Node root,ArrayList<Integer> list)
+    {
+        if(root == null)
+           return;
+           
+          
+              if(root.left != null && root.right == null)
+              list.add(root.left.data);
+               if(root.left == null && root.right != null)
+              list.add(root.right.data);
+              
+              
+              sol(root.left,list);
+              sol(root.right,list);
+              
     }
 }
+
