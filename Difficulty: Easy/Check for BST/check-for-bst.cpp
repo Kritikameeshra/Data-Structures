c1aeff7423631b6abebaba64085a4d596cf8d931
Kritika Kumari Mishra <144877304+Kritikameeshra@users.noreply.github.com>
2025-02-11 20:07:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check for BST/check-for-bst.cpp
+++ Difficulty: Easy/Check for BST/check-for-bst.cpp
@@ -18,13 +18,41 @@ struct Node {
 
 // } Driver Code Ends
 
-class Solution
-{
-public:
-    int prev = INT_MIN;
-    bool isBST(Node *root)
-    {
-        return !root ? true : isBST(root->left) and (prev < root->data) and (prev = root->data) and isBST(root->right);
+class Solution {
+  public:
+    // Function to check whether a Binary Tree is BST or not.
+    bool isLeaf(Node* root){
+        if(!root)
+            return false;
+        if(root->left || root->right)
+            return false;
+        return true;
+    }
+    
+    
+    pair<int,int> isBST_helper(Node* root){
+        if(!root)
+            return {INT_MAX , INT_MIN};
+        if(isLeaf(root))
+            return {root->data , root->data};
+        
+        pair<int,int> l = isBST_helper(root->left);
+        pair<int,int> r = isBST_helper(root->right);
+        
+        if((root->data <= l.second) || (root->data >= r.first))
+            return {INT_MIN , INT_MAX};
+        
+        return {min({l.first,root->data,r.first}) , max({l.second,root->data,r.second})};
+            
+    }
+    
+    bool isBST(Node* root) {
+        // Your code here
+        pair<int,int> p = isBST_helper(root);
+        if((p.first == INT_MIN) || (p.second == INT_MAX))
+            return false;
+        return true;
+        
     }
 };
 
@@ -123,6 +151,7 @@ int main() {
 
         else
             cout << "false\n";
+        cout << "~" << endl;
     }
     return 0;
 }
